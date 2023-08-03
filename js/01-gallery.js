546866b8a14790f2744318ab5af16f0c7deea22e
Schumellman <igor.bondarchuk.1511@gmail.com>
2023-08-03 20:41:25 +0200

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
 
+const container = document.querySelector('.gallery');
 
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}"  data-source="${original}" alt="${description}" width=300>
+        </a>
+    </li>`).join('');
+}
+createMarkup(galleryItems);
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+container.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+    evt.preventDefault();
+    const origImg = evt.target.dataset.source;
+    if (evt.target === evt.currentTarget) {
+        return;
+    }
+    onOpenOrigImg(origImg);
+}
+
+function onOpenOrigImg(origImg) {
+    const instance = basicLightbox.create(`
+	<div>
+        <img src="${origImg}" width="1000" heigth="600">
+    </div>
+`,
+    { onShow: (instance) => { document.addEventListener('keydown', onEvtKey) } },
+    { closeShow: (instance) => { document.removeEventListener('keydown', onEvtKey) } })
+    instance.show();
+
+    function onEvtKey(e) {
+        if (e.code === 'Escape')
+            return instance.close();
+    }
+}
