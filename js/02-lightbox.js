534d38cad06249074f8bf5e65b13a26228988b6f
Schumellman <igor.bondarchuk.1511@gmail.com>
2023-08-04 19:10:20 +0200

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const listBox = document.querySelector('ul.gallery');
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"></a>
+    </li>`).join();
+}
+createMarkup(galleryItems);
+
+listBox.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+listBox.addEventListener('click', onClickOnImg);
+
+function onClickOnImg(evt) {
+    evt.preventDefault();
+    if (evt.target === evt.currentTarget) {
+        return;
+    }
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250
+    })
+}
