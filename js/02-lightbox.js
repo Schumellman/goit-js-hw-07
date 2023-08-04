f78517f49029dc76eb9a67d331ee93e46feac560
Schumellman <igor.bondarchuk.1511@gmail.com>
2023-08-04 20:44:45 +0200

    fixed 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,7 +8,7 @@ function createMarkup(arr) {
         `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}"></a>
-    </li>`).join();
+    </li>`).join('');
 }
 createMarkup(galleryItems);
 
